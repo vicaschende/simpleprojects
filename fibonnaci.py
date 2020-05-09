01ef6795bdb7ca3dab25d2f2fa1a329b2ca61757
Victor Bello-Ochende <vicaschende@gmail.com>
2020-05-09 03:37:41 +0100

    Update fibonnaci.py

--- fibonnaci.py
+++ fibonnaci.py
@@ -1,1 +1,18 @@
+ = int(input("Enter an integer value: "))
+i = abs(i)
+n = 0
+# g_r stands for "the golden ratio"
+g_r = 1.618034
+x = 1 - g_r
+# using the golden ratio to calculate the fibonacci sequence;
+while n <= i:
+     term = (pow(g_r, n) - pow(x, n))/(2.236068)
 
+     term = round(term, 0)
+
+     print(term)
+
+     n += 1
+
+
+print ("done with sequence")
